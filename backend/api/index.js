9c9fa64dc1bf937b551d01148e8db171f3758790
Amank Arya <reachoutamank@gmail.com>
2025-04-08 21:46:31 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // api's
 app.use("/api/v1/user", userRoute);
